7c7469e0c7b0ad20e91d18ca76656c19e3e6077e
ZhangHan <ZhangHan@DESKTOP-3N3C6FE>
2018-04-22 16:21:02 +0800

    注释版1.1.1

--- readAMCASF/posture.h
+++ readAMCASF/posture.h
@@ -12,8 +12,11 @@ public:
 		//If a particular bone does not have a certain degree of freedom, 
 		//the corresponding rotation is set to 0.
 		//The order of the bones in the array corresponds to their ids in .ASf file: root, lhipjoint, lfemur, ...
+		//骨骼旋转变换
 		vector bone_rotation[MAX_BONES];
+		//骨骼位置变换
 		vector bone_translation[MAX_BONES];
+		//骨骼长度
 		float bone_length[MAX_BONES];
 public:
 	posture(void);
